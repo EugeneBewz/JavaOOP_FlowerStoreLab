d68b5a882391a39fc19a70501f57acfcd90875fb
Eugene Bewz <bevz.pn@ucu.edu.ua>
2023-10-28 18:27:02 +0300

    Initial Commit

--- src/main/java/ua/edu/ucu/apps/lab7/decorations/PaperDecorator.java
+++ src/main/java/ua/edu/ucu/apps/lab7/decorations/PaperDecorator.java
@@ -3,7 +3,11 @@ package ua.edu.ucu.apps.lab7.decorations;
 import ua.edu.ucu.apps.lab7.items.Item;
 
 public class PaperDecorator extends ItemDecorator {
-    private Item item;
+    private final Item item;
+
+    public PaperDecorator(Item item) {
+        this.item = item;
+    }
 
     public double getPrice() {
         return 13 + item.price();
@@ -11,7 +15,7 @@ public class PaperDecorator extends ItemDecorator {
 
     public String getDescription() {
         return item.getDescription();
-    };
+    }
 
     @Override
     public double price() {
--- src/main/java/ua/edu/ucu/apps/lab7/payment_strategy/Payment.java
+++ src/main/java/ua/edu/ucu/apps/lab7/payment_strategy/Payment.java
@@ -1,5 +1,5 @@
 package ua.edu.ucu.apps.lab7.payment_strategy;
 
 public interface Payment {
-    abstract boolean pay(double price);
+    boolean pay(double price);
 }
